0f7423c22ad9d6f1ea6dad98d5ee18d354ee1a19
HomePC <HomePC@PC>
2024-07-31 22:07:34 -0700

    Add project files.

---
+++ assign/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign
+{
+    public class Course
+    {
+        public string courseName;
+        public int courseCredit;
+        public char grade;
+        public char[] values = { 'A', 'B', 'C', 'D', 'F' };
+
+    }
+    
+}
---
+++ assign/Program.cs
@@ -0,0 +1,31 @@
+﻿using assign;
+using System.Runtime.Serialization;
+// See https://aka.ms/new-console-template for more information
+
+
+
+
+Console.WriteLine("Welcome to user registration portal");
+Console.ReadKey();
+
+Console.WriteLine("Enter the number of courses");
+int nuofcourse= int.Parse(Console.ReadLine());
+List<Course> courses = new List<Course>();
+for (int i = 0; i < nuofcourse; i++)
+{
+    Course newCourse = new Course();
+    Console.WriteLine("please input course name" + (i + 1));
+    newCourse.courseName = Console.ReadLine();
+    Console.WriteLine("please input course credit" + (i + 1));
+    newCourse.courseCredit = int.Parse(Console.ReadLine());
+    Console.WriteLine("please input course grade" + (i + 1));
+    newCourse.grade = Char.Parse(Console.ReadLine());
+}
+
+Console.Clear();
+
+foreach (Course course in courses)
+{
+   
+}
+
